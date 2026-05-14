8245678b6270a0324f8c1f01a1f128fdd4a87d83
surajit <samantasurajit@533.com>
2026-05-14 22:47:09 +0530

    add sucessfulyiiiiil mmmm  pppppp

--- src/main/java/com/social/Service/imp/EmailServiceImp.java
+++ src/main/java/com/social/Service/imp/EmailServiceImp.java
@@ -1,27 +1,47 @@
 package com.social.Service.imp;
 
 import com.social.Service.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
+import sibApi.TransactionalEmailsApi;
+import sibModel.*;
+import sendinblue.ApiClient;
+import sendinblue.Configuration;
+import sendinblue.auth.ApiKeyAuth;
+import java.util.Collections;
 
 @Service
 public class EmailServiceImp implements EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${BREVO_API_KEY}")
+    private String apiKey;
+
     @Override
     public void sendOtpEmail(String toEmail, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
 
-        message.setTo(toEmail);
-        message.setSubject("Verify Your Social Media Account Registration");
-        message.setText("Welcome to our platform!\n\n" +
-                "Your verification OTP code is: " + otp + "\n\n" +
-                "This code is confidential and will expire in 5 minutes.");
+        // Configure API key authorization: api-key
+        ApiKeyAuth apiKeyAuth = (ApiKeyAuth) defaultClient.getAuthentication("api-key");
+        apiKeyAuth.setApiKey(apiKey);
 
-        mailSender.send(message);
+        TransactionalEmailsApi apiInstance = new TransactionalEmailsApi();
+
+        SendSmtpEmail sendSmtpEmail = new SendSmtpEmail();
+
+        // 1. Sender: Must be the email verified in your Brevo Dashboard
+        sendSmtpEmail.setSender(new SendSmtpEmailSender().email("your-email@gmail.com"));
+
+        // 2. Receiver
+        sendSmtpEmail.setTo(Collections.singletonList(new SendSmtpEmailTo().email(toEmail)));
+
+        // 3. Subject and Content
+        sendSmtpEmail.setSubject("Verification OTP");
+        sendSmtpEmail.setHtmlContent("Your OTP code is: <b>" + otp + "</b>");
+
+        try {
+            apiInstance.sendTransacEmail(sendSmtpEmail);
+        } catch (Exception e) {
+            throw new RuntimeException("Brevo API Connection Failed: " + e.getMessage());
+        }
     }
 }
