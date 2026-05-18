5f68d2875b45acd61cb11a331b6b361ae34c7340
surajit <samantasurajit@533.com>
2026-05-19 00:21:58 +0530

    all done mmmm mmmm

--- src/main/java/com/social/Config/JwtProvider.java
+++ src/main/java/com/social/Config/JwtProvider.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @Service
 public class JwtProvider {
 
-    @Value("${jwt.secret:njnjhgugyyvhvgcfc7h5ytghfgt4fgg4ryy76hfft3dfgd33ddr2cg6gb4ghui8fgxw}")
+    @Value("${JWT_SECRET}")
     private String secretKey;
 
     // Helper method to guarantee a cryptographically secure key of proper length
